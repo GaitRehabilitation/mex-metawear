b383fe174e375dde0a673c8c564c77a9970eba9f
Michael Pollind <mpollind@gmail.com>
2018-12-17 15:08:01 -0800

    added warble

--- 3rdparty/mbientlab.cmake
+++
@@ -1,5 +0,0 @@
-file(GLOB_RECURSE Mbient_SOURCE MetaWear-SDK-Cpp/src/metawear/**/*.cpp)
-file(GLOB_RECURSE Mbient_HEADERS MetaWear-SDK-Cpp/src/metawear/**/*.h)
-
-add_library(extern_metawear ${Mbient_SOURCE} ${Mbient_HEADERS})
-target_include_directories(extern_metawear PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/MetaWear-SDK-Cpp/src)
